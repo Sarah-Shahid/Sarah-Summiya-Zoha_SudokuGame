8c397808110315a9dc005f386ff9260566d5f786
Sarah Shahid <sarahshahid2006@gmail.com>
2025-11-22 18:28:20 +0500

    The program body having just main.

---
+++ programbody.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+//function prototyping.
+
+using namespace std;
+int main ()
+{
+	srand(time(0));
+    int score = 0;
+    return 0;
+}
+//define functions here.
